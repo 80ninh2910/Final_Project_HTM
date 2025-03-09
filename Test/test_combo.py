b03d705ae14954f327892a74a322ee08151a71e8
80ninh2910 <ninhqa234112e@st.uel.edu.vn>
2025-03-09 18:11:18 +0700

    Create test_combo.py

---
+++ Test/test_combo.py
@@ -0,0 +1,10 @@
+from libs.JSON_File_Factory import JsonFileFactory
+from models.CONCESSION.Combo import Combo
+
+combos=[]
+c1=Combo("Combo Solo",90000)
+c2=Combo("Combo Couple",110000)
+combos.extend([c1,c2])
+
+jff=JsonFileFactory()
+jff.write_data(combos,"../database/Combo.json")
