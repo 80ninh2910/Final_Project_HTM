b2f28fee9c4f1549f86b7def6cb3a5f8adcb67d0
80ninh2910 <ninhqa234112e@st.uel.edu.vn>
2025-03-09 18:11:22 +0700

    Update Popcorn.py

--- models/CONCESSION/Popcorn.py
+++ models/CONCESSION/Popcorn.py
@@ -3,9 +3,9 @@ from numpy.f2py.symbolic import number_types
 
 
 class Popcorn:
-    def __init__(self, PName, PQuantity, PPrice):
+    def __init__(self, PName, PPrice):
         self.PName=PName
-        self.PQuantity=PQuantity
+
         self.PPrice=PPrice
     def __str__(self):
-        return f"{self.PName}\t{self.PQuantity}\t{self.PPrice}"
+        return f"{self.PName}\t{self.PPrice}"
