88b4d811ab99f30a5993b9801255ff9db664156b
tuantubatpham <doanquocvinhcute7@gmail.com>
2025-03-09 11:58:14 +0700

    hi

--- ui/Popcorn/BuyPopcornEx.py
+++ ui/Popcorn/BuyPopcornEx.py
@@ -7,6 +7,18 @@ class BuyPopcornEx(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)  # Initialize UI
-
+        self.setupSignalAndSlots()
+        self.mainwindow=None
     def showWindow(self):
         self.show()  # Show this window properly
+
+    def setupSignalAndSlots(self):
+        self.pushButtonHome.clicked.connect(self.home)
+    def home(self):
+        from ui.Main.MainEx import MainEx
+
+        if self.mainwindow is None:
+            self.mainwindow = MainEx()
+
+        self.mainwindow.show()
+        self.close()
