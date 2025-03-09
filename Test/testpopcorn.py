48c568050da18923bd9ad166dffd1b7bb1248320
80ninh2910 <ninhqa234112e@st.uel.edu.vn>
2025-03-09 18:11:13 +0700

    Create testpopcorn.py

---
+++ Test/testpopcorn.py
@@ -0,0 +1,10 @@
+from libs.JSON_File_Factory import JsonFileFactory
+from models.CONCESSION.Popcorn import Popcorn
+
+pops=[]
+p1=Popcorn("Original",50000)
+p2=Popcorn("Cheese",60000)
+p3=Popcorn("Caramel",60000)
+pops.extend([p1,p2,p3])
+jff=JsonFileFactory()
+jff.write_data(pops,"../database/Popcorn.json")
