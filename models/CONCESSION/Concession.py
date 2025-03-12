89bee98b79b0616bf82ea90ac840ca2686e061fe
80ninh2910 <ninhqa234112e@st.uel.edu.vn>
2025-03-12 20:01:37 +0700

    Update Concession.py

--- models/CONCESSION/Concession.py
+++ models/CONCESSION/Concession.py
@@ -1,5 +1,6 @@
 class CON:
-    def __init__(self, popcorn,Pquantity, beverage,Bquantity, combo,Cquantity):
+    def __init__(self,CId, popcorn=None,beverage=None,combo=None,Pquantity=None, Bquantity=None, Cquantity=None):
+        self.CId=CId
         self.popcorn = popcorn
         self.beverage = beverage
         self.combo = combo
@@ -7,7 +8,7 @@ class CON:
         self.Bquantity=Bquantity
         self.Cquantity=Cquantity
     def total_price(self,Pprice,Bprice,Cprice):
-        return self.Pquantity*Pprice +self.Bquantity*Bprice+self.Cquantity*Cprice
+        return self.Pquantity*Pprice + self.Bquantity*Bprice + self.Cquantity*Cprice
     def __str__(self):
 
         return f"Popcorn: {self.popcorn} (Qty: {self.Pquantity}), Beverage: {self.beverage} (Qty: {self.Bquantity}), Combo: {self.combo} (Qty: {self.Cquantity})"
