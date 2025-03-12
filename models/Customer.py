87d0dd1514e8d20151c24c3f9beec50b486e4fc6
80ninh2910 <ninhqa234112e@st.uel.edu.vn>
2025-03-12 20:01:35 +0700

    Update Customer.py

--- models/Customer.py
+++ models/Customer.py
@@ -1,7 +1,8 @@
 class Customer:
-    def __init__(self, email, username, password):
+    def __init__(self,CId, email, username, password):
+        self.CId=CId
         self.email=email
         self.username=username
         self.password=password
     def __str__(self):
-        return f"{self.email}\t{self.username}\t{self.password}"
+        return f"{self.CId}\t{self.email}\t{self.username}\t{self.password}"
